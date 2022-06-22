a99766c50ab7096eb7cd9df768c2551b26ce7abf
EdenMl <emalo@wevox.eu>
2022-06-22 21:35:36 +0200

    [UPD] 22.06

---
+++ Sport-Club-B3/Datas.swift
@@ -0,0 +1,20 @@
+//
+//  Datas.swift
+//  Sport-Club-B3
+//
+//  Created by Eden MALO on 22/06/2022.
+//
+
+import SwiftUI
+
+struct Datas: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Datas_Previews: PreviewProvider {
+    static var previews: some View {
+        Datas()
+    }
+}
---
+++ Sport-Club-B3/elements/SquareWebPicture.swift
@@ -0,0 +1,20 @@
+//
+//  SquareWebPicture.swift
+//  Sport-Club-B3
+//
+//  Created by Eden MALO on 22/06/2022.
+//
+
+import SwiftUI
+
+struct SquareWebPicture: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SquareWebPicture_Previews: PreviewProvider {
+    static var previews: some View {
+        SquareWebPicture()
+    }
+}
